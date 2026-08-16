d0c109096f32ba8e3167a41bb0b160f8eae654fb
kari-nami <kazakova.katya@outlook.com>
2026-08-16 23:27:25 +0700

    cards open and close, but no cool logic yet

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,43 @@
 import './App.css'
 import shuffle from "./Shuffle.js";
+import {useEffect, useRef, useState} from "react";
 
 function App() {
+
   const cardsUnshuffled = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10]
-  const cards = shuffle(cardsUnshuffled)
+  const cards = useRef(shuffle(cardsUnshuffled)).current
+  const [openCards, setOpenCards] = useState(new Array(cards.length).fill(false))
+  const [cardsOpen, setCardsOpen] = useState(0)
+
+  function handleCardClick(cardIndex) {
+    setCardsOpen((previous) => previous + 1)
+    console.log(cardsOpen)
+
+    if (cardsOpen >= 2) {
+
+      setOpenCards(new Array(cards.length).fill(false))
+      setCardsOpen(0)
+    }
+
+    else {
+
+      const newOpenCards = [...openCards]
+      newOpenCards[cardIndex] = !newOpenCards[cardIndex]
+      setOpenCards(newOpenCards)
+      console.log(newOpenCards)
+    }
+  }
 
   return (
     <>
       <div className="game-area">
         {cards.map((card, index) => {
           return (
-            <div key={index} id={card} className="memory-card" >
-              {card}
+            <div key={index} id={card}
+                 className={`memory-card ${openCards[index] === true ? "open" : ""}`}
+                 onClick={() => handleCardClick(index)}
+            >
+              {openCards[index] === true ? card : ""}
             </div>
           )
         })}
